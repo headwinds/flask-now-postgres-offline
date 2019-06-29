b4bc00b7a912c9fa6d7862e74cfff3ce11698954
Brandon Flowers <brandonflowers@gmail.com>
2019-06-29 19:55:13 -0400

    trans up

---
+++ blueprints/transaction/get_transaction.py
@@ -0,0 +1,58 @@
+from flask import Blueprint, redirect, url_for, render_template, request, session, json, jsonify
+from helpers.helpers_transactions import transaction_valid, add_transaction
+
+
+def get_transaction(form):
+    if request.is_json is True:
+        json_data = request.get_json()
+        title = json_data["title"]
+        type = json_data["type"]
+        cost = json_data["cost"]
+        transaction_json = json_data["transaction_json"]
+
+        if transaction_valid(title, type, cost, transaction_json):
+            result = add_transaction(title, type, cost, transaction_json)
+
+            if result is True:
+                return jsonify({"data": {"message": "success", "status": 200}})
+            else:
+                return jsonify({
+                    "data": {
+                        "message": "invalid transaction database error",
+                        "status": 200
+                    }
+                })
+
+        return jsonify({
+            "data": {
+                "message": "invalid transaction failed validation",
+                "status": 200
+            }
+        })
+
+    elif form.validate():
+        title = request.form["title"]
+        type = request.form["type"]
+        cost = request.form["cost"]
+        transaction_json = request.form["transaction_json"]
+
+        if transaction_valid(title, type, cost, transaction_json):
+
+            result = add_transaction(title, type, cost, transaction_json)
+
+            if result is True:
+                return jsonify({"data": {"message": "success", "status": 200}})
+            else:
+                return jsonify({
+                    "data": {
+                        "message": "invalid transaction failed validation",
+                        "status": 200
+                    }
+                })
+
+    return jsonify({
+        "data": {
+            "message": "invalid transaction failed both form and json",
+            "status": 200
+        }
+    })
--- blueprints/transaction_blueprint.py
+++ blueprints/transaction_blueprint.py
@@ -1,63 +1,13 @@
 from flask import Blueprint, redirect, url_for, render_template, request, session, json, jsonify
 from helpers.helpers_transactions import transaction_valid, add_transaction
 from forms.transaction_form import TransactionForm
+from blueprints.transaction.post_transaction import post_transaction
 
 transaction_blueprint = Blueprint("transaction", __name__)
 
-
-def post_transaction(form):
-    if request.is_json is True:
-        json_data = request.get_json()
-        title = json_data["title"]
-        type = json_data["type"]
-        cost = json_data["cost"]
-        transaction_json = json_data["transaction_json"]
-
-        if transaction_valid(title, type, cost, transaction_json):
-            result = add_transaction(title, type, cost, transaction_json)
-
-            if result is True:
-                return jsonify({"data": {"message": "success", "status": 200}})
-            else:
-                return jsonify({
-                    "data": {
-                        "message": "invalid transaction database error",
-                        "status": 200
-                    }
-                })
-
-        return jsonify(
-            {"data": {
-                "message": "invalid transaction failed validation",
-                "status": 200
-            }})
-
-    elif form.validate():
-        title = request.form["title"]
-        type = request.form["type"]
-        cost = request.form["cost"]
-        transaction_json = request.form["transaction_json"]
-
-        if transaction_valid(title, type, cost, transaction_json):
-
-            result = add_transaction(title, type, cost, transaction_json)
-
-            if result is True:
-                return jsonify({"data": {"message": "success", "status": 200}})
-            else:
-                return jsonify({
-                    "data": {
-                        "message": "invalid transaction failed validation",
-                        "status": 200
-                    }
-                })
-
-    return jsonify({"data": {"message": "invalid transaction failed both form and json", "status": 200}})
-
-
 # -------- API Settings --------- #
-@transaction_blueprint.route(
-    '/api/transactions', methods=['GET', 'POST', 'PUT', 'DELETE', 'PATCH'])
+@transaction_blueprint.route('/api/transactions',
+                             methods=['GET', 'POST', 'PUT', 'DELETE', 'PATCH'])
 def api_transactions():
     if session.get('logged_in'):
 
@@ -65,13 +15,24 @@ def api_transactions():
 
         if request.method == 'POST':
             return post_transaction(form)
+        elif request.method == 'PUT':
+            return put_transaction(form)
+        elif request.method == 'PATCH':
+            return patch_transaction(form)
+        elif request.method == 'DELETE':
+            return delete_transaction(form)
 
-    return jsonify({"data": {"message": "invalid transaction not logged in", "status": 200}})
+    return jsonify({
+        "data": {
+            "message": "invalid transaction not logged in",
+            "status": 200
+        }
+    })
 
 
 # -------- Test Settings --------- #
-@transaction_blueprint.route(
-    '/transactions', methods=['GET', 'POST', 'PUT', 'DELETE', 'PATCH'])
+@transaction_blueprint.route('/transactions',
+                             methods=['GET', 'POST', 'PUT', 'DELETE', 'PATCH'])
 def transactions():
     if session.get('logged_in'):
         if request.method == 'POST':
